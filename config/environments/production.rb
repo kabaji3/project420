b52a0d92aa25f6f0c7238f70010efee7fe570e35
葛原哲 <kuzuharasatoru@katsuraharaakira-no-MacBook-Air.local>
2017-09-04 11:33:07 +0900

    host に、wwwをつけた

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,13 +88,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'fudawari.com' }
+  config.action_mailer.default_url_options = { host: 'www.fudawari.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
-    :port => '587',
+    :port => 587,
     :domain => 'smtp.gmail.com',
     :user_name => ENV["GOOGLE_MAILADDRESS_PRODUCTION"],
     :password => ENV["GOOGLE_PASSWORD_PRODUCTION_1"],
