a97d887a2de840cbd308c8a766ed3d7e39874e4a
葛原哲 <kuzuharasatoru@katsuraharaakira-no-MacBook-Air.local>
2017-09-11 14:54:30 +0900

    application.rb 　21行目追加

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Project420test
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+    config.assets.pricompile += %w(*.png *.jpg *.jpeg *.gif *.eot *.woff *.ttf)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
